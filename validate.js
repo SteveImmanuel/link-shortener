e35db63feb35dc9cddef8521f7f3655e36858d5e
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-08-01 21:54:31 +0700

    Add url validation

---
+++ validate.js
@@ -0,0 +1,8 @@
+const yup = require('yup');
+
+const schema = yup.object().shape({
+    url: yup.string().url().required(),
+    slug: yup.string().matches(/^[a-zA-Z0-9]*$/)
+})
+
+module.exports = { schema };

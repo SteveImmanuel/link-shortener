831cb82ea0356bf81e85efe82133a2d75c3e0317
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-08-08 14:24:50 +0700

    Remove unused file

--- server/validate.js
+++
@@ -1,8 +0,0 @@
-const yup = require('yup');
-
-const schema = yup.object().shape({
-    url: yup.string().url().required(),
-    slug: yup.string().matches(/^[a-zA-Z0-9]*$/)
-})
-
-module.exports = { schema };
